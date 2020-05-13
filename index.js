7ce91ef3234bc1c07af8b959c8f1744554028b2f
Your Name <you@example.com>
2020-05-13 09:31:38 -0500

    :tada:

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello",
+  });
+});
+
+app.listen(process.env.PORT || 5000, () => console.log("Listening on port: "));
